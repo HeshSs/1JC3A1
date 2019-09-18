c3d3e4787239666fb31b25825fc94b4c43053d6d
HeshS <46274676+HeshSs@users.noreply.github.com>
2019-09-18 17:07:23 -0400

    updatedAlmost done, Need to solve the floating point error

--- src/Assign_1.hs
+++ src/Assign_1.hs
@@ -38,7 +38,7 @@ cubicDisc q r = (q**3) + (r**2)
  -}
 cubeRoot :: Double -> Double
 cubeRoot a = if (a < 0.0)
-                then  (-a)**(1/3)
+                then  -((-a)**(1/3))
                 else (a)**(1/3)
 
 {- -----------------------------------------------------------------
@@ -63,9 +63,9 @@ cubicT q r = cubeRoot (r - sqrt (cubicDisc q r) )
  - Description: TODO add comments on cubicRealSolutions here 
  -}
 cubicRealSolutions :: Double -> Double -> Double -> Double -> [Double]
-cubicRealSolutions a b c d = if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) < 0)
+cubicRealSolutions a b c d = if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) < 0.0)
                                 then []
-                                else if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) > 0)
+                                else if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) > 0.0)
                                     then [x1]
                                     else [x1, x2, x2]
                                         where    x1 = (cubicS (cubicQ a b c) (cubicR a b c d)) + (cubicT (cubicQ a b c) (cubicR a b c d)) - (b / (3 * a))
@@ -76,14 +76,9 @@ cubicRealSolutions a b c d = if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) < 0
  - -----------------------------------------------------------------
  -}
 
--- TODO Add Test Cases for each of your functions below here
-
-{-
-if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) < 0.0)
-                                then []
-                                else if ((cubicDisc (cubicQ a b c) (cubicR a b c d)) < 0.1e-10 && (cubicDisc (cubicQ a b c) (cubicR a b c d)) > -0.1e-10)
-                                    then [x1, x2, x2]
-                                    else [x1]
-                                        where    x1 = (cubicS (cubicQ a b c) (cubicR a b c d)) + (cubicT (cubicQ a b c) (cubicR a b c d)) - (b / (3 * a))
-                                                 x2 = (- ((cubicS (cubicQ a b c) (cubicR a b c d)) + (cubicT (cubicQ a b c) (cubicR a b c d)))/2) - (b / (3*a)) 
--}
+test1 = cubicRealSolutions 1 2 3 4 == [-1.6506291914393882]
+test2 = cubicRealSolutions 1 (-2) 0 1 == []
+test3 = cubicRealSolutions 2 (-4) (-22) 24 == []
+test4 = cubicRealSolutions 1 0 3 0 == [0.0]
+test5 = cubicRealSolutions 1 2 0 0 == [-2.0,0.0,0.0]
+test6 = cubicRealSolutions 1 2 22 0 == [0.0]
